542d5ed4bdd298245af181a07ef45d725b94c392
Jesse Harlan <jesse.harlan@centralia.edu>
2025-05-08 10:51:45 -0700

    updated for user secrets

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data;
 namespace WebApplication1
 {
     public class Program
@@ -6,9 +9,29 @@ namespace WebApplication1
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            if (builder.Environment.IsDevelopment())
+            {
+                // development server connection
+                builder.Services.AddDbContext<CustomerContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("CustomerContext") ?? throw new InvalidOperationException("Connection string 'CustomerContext' not found.")));
+
+            }
+            else if (builder.Environment.IsProduction())
+            {
+                builder.Services.AddDbContext<CustomerContext>(options =>
+                    options.UseSqlServer(builder.Configuration["ConnectionStrings:CustomerContext"] ?? throw new InvalidOperationException("Connection string 'CustomerContext' not found.")));
+            }
+            else
+            {
+                throw new InvalidOperationException("No valid Environment to Create the Database");
+            }
+
+
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            var db = Environment.GetEnvironmentVariable("PATH");
 
             var app = builder.Build();
 
